c230099a62c8632f053ca3236c2727dd3874efa8
Lucas Tavares <l.tavaresc@hotmail.com>
2022-07-15 00:04:24 -0300

    Foi transferido o layout base do projeto para o React

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom'
+import NavBar from './navbar'
+import SideBar from './sidebar'
+import Stories from './stories'
+import Posts from './feed'
+import FundoMobile from './fundomobile'
+function App() {
+    return (
+        <div>
+            <NavBar />
+            <div class="corpo">
+                <div class="esquerda">
+                    <Stories />
+                    <Posts />
+                </div>
+            <SideBar />
+            </div>
+            <FundoMobile />
+        </div>
+    )
+}
+
+
+ReactDOM.render(<App />, document.querySelector('.root'))
