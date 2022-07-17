a76490bd005ee0c616d88b1a9aa4b0941070b30d
Lucas Tavares <l.tavaresc@hotmail.com>
2022-07-17 15:31:58 -0300

    Agora posts em vídeos também podem ser renderizados.

--- src/posts.js
+++ src/posts.js
@@ -3,8 +3,9 @@ import React from 'react'
 const postContent = [
     { imgUser: "img/meowed.svg", imgPost: "img/gato-telefone.svg", img_whoLiked: "img/respondeai.svg", whoLiked: "respondeai", likes: "101.523" },
     { imgUser: "img/barked.svg", imgPost: "img/dog.svg", img_whoLiked: "img/adorable_animals.svg", whoLiked: "adorable_animals", likes: "99.159" },
+    { imgUser: "img/barked.svg", videoPost:"video/video.mp4", img_whoLiked: "img/adorable_animals.svg", whoLiked: "adorable_animals", likes: "99.159" },
 ]
-    function Post(props) {
+function Post(props) {
 
     const [iconName, setIconName] = React.useState("heart-outline");
     const [iconColor, setIconColor] = React.useState("heart-outline");
@@ -27,55 +28,102 @@ const postContent = [
     function animation() {
         setHeartAnimation("md hydrated heartAnimation transform")
         setInterval(() => {
-             setHeartAnimation("md hydrated heartAnimation")
+            setHeartAnimation("md hydrated heartAnimation")
         }, 1000);
     }
-    return (
-        <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                    <img src={props.imgUser} />
-                    meowed
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
+    if (props.imgPost !== undefined) {
 
-            <div class="conteudo">
-                <div class="imgPost">
-                <img onDoubleClick={likeThePost} src={props.imgPost} />
-                <ion-icon class={heartAnimation} name="heart"></ion-icon>
-                </div>
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                    <div>
-                        <ion-icon class={iconColor} onClick={likeThePost} name={iconName}></ion-icon>
-                        <ion-icon name="chatbubble-outline" ></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
+        return (
+            <div class="post">
+                <div class="topo">
+                    <div class="usuario">
+                        <img src={props.imgUser} />
+                        meowed
                     </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
+                    <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
                     </div>
                 </div>
 
-                <div class="curtidas">
-                    <img src={props.img_whoLiked} />
-                    <div class="texto">
-                        Curtido por <strong>{props.whoLiked}</strong> e <strong>outras {props.likes + userLike} pessoas</strong>
+                <div class="conteudo">
+                    <div class="imgPost">
+                        <img onDoubleClick={likeThePost} src={props.imgPost} />
+                        <ion-icon class={heartAnimation} name="heart"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="fundo">
+                    <div class="acoes">
+                        <div>
+                            <ion-icon class={iconColor} onClick={likeThePost} name={iconName}></ion-icon>
+                            <ion-icon name="chatbubble-outline" ></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="curtidas">
+                        <img src={props.img_whoLiked} />
+                        <div class="texto">
+                            Curtido por <strong>{props.whoLiked}</strong> e <strong>outras {props.likes + userLike} pessoas</strong>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    if (props.videoPost !== undefined) {
+
+        return (
+            <div class="post">
+                <div class="topo">
+                    <div class="usuario">
+                        <img src={props.imgUser} />
+                        meowed
+                    </div>
+                    <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="conteudo">
+                    <div class="imgPost">
+                        <video src={props.videoPost} autoPlay
+                            muted loop></video>
+                        <ion-icon class={heartAnimation} name="heart"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="fundo">
+                    <div class="acoes">
+                        <div>
+                            <ion-icon class={iconColor} onClick={likeThePost} name={iconName}></ion-icon>
+                            <ion-icon name="chatbubble-outline" ></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="curtidas">
+                        <img src={props.img_whoLiked} />
+                        <div class="texto">
+                            Curtido por <strong>{props.whoLiked}</strong> e <strong>outras {props.likes + userLike} pessoas</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 function Posts() {
     return (
         <div class="posts">
-            {postContent.map(item => (<Post imgUser={item.imgUser} imgPost={item.imgPost} img_whoLiked={item.img_whoLiked} whoLiked={item.whoLiked} likes={item.likes} />))}
+            {postContent.map(item => (<Post imgUser={item.imgUser} imgPost={item.imgPost} videoPost={item.videoPost} img_whoLiked={item.img_whoLiked} whoLiked={item.whoLiked} likes={item.likes} />))}
         </div>
     )
 }
